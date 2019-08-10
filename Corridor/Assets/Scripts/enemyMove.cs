3b3c2ce555cb7da7198196738f64123d63f7eade
Shoouuni <39516436+Shoouuni@users.noreply.github.com>
2019-08-10 18:55:22 +0200

    Move script

---
+++ Corridor/Assets/Scripts/enemyMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour
+{
+    public Transform Player;
+    public int MoveSpeed = 4;
+    public int MinDist = 5;
+    public int MaxDist = 10;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //Moves towrd player
+        transform.LookAt(Player);
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+               // Destroy(this.gameObject, 1f);
+
+            }
+        }
+    }
+}
